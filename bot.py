2b124c037de3347d7e11028f6ad5fffc6e5459fc
hsr201222-cyber <hsr201222@gmail.com>
2026-05-19 19:42:49 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,22 @@
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Шарль работает!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
+# Вызови эту функцию в самом начале
+keep_alive()
+
 import time
 import urllib.request
 import json
